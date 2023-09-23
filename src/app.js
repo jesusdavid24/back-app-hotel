433145900fc99083a93bb927eba83336009e43d4
jesus bravo <jd_bravo24@hotmail.com>
2023-09-23 06:09:28 -0500

    fist commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express');
+const configExpress = require('./config/express');
+//const connect = require('./config/database');
+//const routes = require('./routes');
+
+const app = express();
+// connect()
+
+
+//setup express
+configExpress(app);
+//setup routes
+//routes(app);
+
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const server = require('./app')
+
+
+var port = process.env.PORT || 3005;
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+})
